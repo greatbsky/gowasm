322d198ed71b4b72c0cb17ec4cdfa80fe067e46c
architect.bian <greatarchitect@163.com>
2018-11-14 17:03:40 +0800

    add some demo code

--- Main.go
+++ Main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
+	hello()
+}
+
+func hello() {
 	fmt.Println("Hello, WebAssembly!")
 }
+
+//func main() {
+//	c := make(chan struct{}, 0)
+//	add := func(i []js.Value) {
+//		js.Global.Set("output", js.ValueOf(i[0].Int()+i[1].Int()))
+//	}
+//	js.Global.Set("add", js.NewCallback(add))
+//	<-c
+//}
+
+//func jsDom() {
+//	c := make(chan struct{}, 0)
+//	cb = js.NewCallback(func(args []js.Value) {
+//		move := js.Global.Get("document").Call("getElementById", "body").Get("value").Int()
+//		fmt.Println(move)
+//	})
+//	js.Global.Get("document").Call("getElementById", "myText").Call("addEventListener", "input", cb)
+//	// The goal of the channel is to wait indefinitly
+//	// Otherwise, the main function ends and the wasm modules stops
+//	<-c
+//}
+
